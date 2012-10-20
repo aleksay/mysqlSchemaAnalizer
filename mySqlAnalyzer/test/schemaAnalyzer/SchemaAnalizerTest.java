769b337b60f08db960b315b34ce346fc672664d0
aleksay <ale@Skull.debdom>
2012-10-20 14:10:34 +0000

    junit configuration test

---
+++ mySqlAnalyzer/test/schemaAnalyzer/SchemaAnalizerTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author ale
+ *
+ */
+public class SchemaAnalizerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link schemaAnalyzer.SchemaAnalyzer#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
